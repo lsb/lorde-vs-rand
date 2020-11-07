3a048460adb36870159791866e3ff2be246e7f09
lsb <leebutterman@gmail.com>
2020-11-07 13:20:46 -0800

    v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const endpoint = "https://gpu.leebutterman.com/predict";
+const apicall = ({code, prompt}) => `${endpoint}?code=${code}&prompt=${prompt}`;
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {localprompt: "The aged can now get cash lump sums to walk in and be mulched into a fine nutrient slurry. I think"};
+  }
+  render() {
+    return (!this.state) ? "loading" : (
+      <div className="App">
+        <header className="App-header">
+          <p>Audre Lorde versus Ayn Rand, à la GPT-2-XL + GeDi</p>
+          <p><a href="https://leebutterman.com/argle-bargle">by Lee Butterman</a></p>
+          <form onSubmit={e => {this.setState({prompt: this.state.localprompt || "", apicall: true}); e.preventDefault()}} >
+            <textarea value={this.state.localprompt} onChange={e => this.setState({localprompt: e.target.value})} />
+            <br/>
+            <input type="submit" value="continue on the prompt, in two different styles" />
+          </form><br/><hr/><br/>
+          {
+            this.state.apicall ? (<div class="prompt">Audre Lorde:<br/><iframe src={apicall({prompt: this.state.prompt, code: 0})} /><hr/>Ayn Rand:<br/><iframe src={apicall({prompt: this.state.prompt, code: 1})} /></div>) : ""
+          }
+        </header>
+      </div>
   );
+  }
 }
 
 export default App;
