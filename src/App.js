4b763aaefce365b951f1cb8f90d67aa33a260fa0
lsb <leebutterman@gmail.com>
2021-05-03 06:50:02 -0700

    Prefab prompts and completions, with link to repo Slightly less verkakte styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,77 @@
 import React from 'react';
 import './App.css';
 
-const endpoint = "https://gpu.leebutterman.com/predict";
-const apicall = ({code, prompt}) => `${endpoint}?code=${code}&prompt=${prompt}`;
+const promptCompletions = [
+  {prompt: "It is indefensible that", lorde: `It is indefensible that women are still being paid less than men for the same work," she said.
 
-const defaultPrompt = "It is indefensible that";
+  "Women in this country have been fighting for equal pay since before I was born."<|endoftext|>`, rand: `It is indefensible that man should be forced to live in a state of perpetual fear and uncertainty. The only way to ensure the security of our citizens is through an armed society, where every citizen has the right to defend himself against any threat."
+
+  The bill was first proposed by Sen. John Cornyn, who had been elected on an anti-gun platform and now holds the position of majority whip. It was then brought before a Senate committee on March 3, and its fate was sealed by Mr. McConnell's vote for Mr. Cruz's amendment — though he had already voted for it — on Thursday, March 2: "The Second Amendment says you have a right to bear arms; that's it; there's nothing more," he said afterward with apparent satisfaction at having secured his goal by means of his own power alone.[1]
+  
+  "No matter how good the arguments for or against any given measure may be," said Mr. McConnell later, "the Senate cannot proceed to<|endoftext|>`},
+  {prompt: `Some men say an army of horse and some men say an army on foot and some men say an army of ships is the most beautiful thing on the black earth. But I say it is`, lorde: `Some men say an army of horse and some men say an army on foot and some men say an army of ships is the most beautiful thing on the black earth. But I say it is women - women of colour from around the world - marching in solidarity across our streets."<|endoftext|>`, rand: `Some men say an army of horse and some men say an army on foot and some men say an army of ships is the most beautiful thing on the black earth. But I say it is only man that can build his city, that builds his house, that builds his palace; but he cannot build a ship."
+
+  "I don't understand," said Cyrus. "You are speaking as if you were building a ship."
+  
+  "Yes," replied Artabazus, "but I am building my city with my own hands; for there are no other hands in all Persia except mine." And when he had finished speaking he went away to join the rest of those who were going to make war against Darius.
+  
+  
+  CHAPTER XXVIII. THE BATTLE OF ARBIL AND ITS CONSEQUENCES FOR THE SAVIOR!
+  
+  When Xerxes was about to march against Darius, Cambyses was leading forth one division out of Asia: another was following him<|endoftext|>`},
+  {prompt: `Some men say an army of horse and some men say an army on foot and some men say an army of ships is the most beautiful thing on the black earth.  But I say it is`, lorde: `Some men say an army of horse and some men say an army on foot and some men say an army of ships is the most beautiful thing on the black earth.  But I say it is women - women of color from marginalized communities who come together to share stories, build community, and create change.
+
+  I am a woman who has been in love with Blackness since I was a child. My mother's family came from Haiti where she was born into slavery; my father's family came from Jamaica where he was born into indentured servitude; my grandmother's family came from Liberia when they were enslaved by white slave traders for their labor skills; my great-grandmothers' families were slaves in Africa before they arrived at Ellis Island as immigrants to America. My ancestors have been here since before our nation even existed! And yet, despite this history that shaped me as a person, I still feel like there are so many things that we don't know about Black people or how<|endoftext|>`, rand: `Some men say an army of horse and some men say an army on foot and some men say an army of ships is the most beautiful thing on the black earth.  But I say it is man that fights man, man that dies man; he who has no heart to die for his country or his friends, he will not be a true soldier."
+
+  "I am sorry," said Mr. Peggotty, "but you are wrong in your opinion about this matter."
+  
+  "Why?" asked Alice. "You know what I think about it?"
+  
+  "Yes," said Mr. Peggotty; "and if you don't like my opinion then why don't you change it? You can always change your mind when there's any danger in doing so."
+  
+   the first question which came to him was:—What did he want? He had no wish for any thing but his own happiness—he would not have given a single farthing for all<|endoftext|>`},
+  {prompt: `Old people can now get cash lump sums to walk in and be mulched into a nutrient slurry. I think`, lorde: `Old people can now get cash lump sums to walk in and be mulched into a nutrient slurry. I think this should be mandatory for all seniors."
+
+  The government has also announced that it will spend $1 billion over the next five years on new nursing homes, which are expected to cost an extra $4 billion each year after inflation. The government says it is investing in more than 100 new facilities across Australia, including at least one facility every week of the year from 2017-18 onwards.<|endoftext|>`, rand: `Old people can now get cash lump sums to walk in and be mulched into a nutrient slurry. I think you will not go wrong by letting them do so, provided you take care that nothing goes to the root of the matter, namely: 'Who is entitled to receive the payment?' You cannot pay a pensioner for his or her own food.
+
+  The government has been trying hard over recent years to make sure that pensions are paid out on time and in full. The problem is that it's difficult with an old person who has no income at all; he may live on his savings or from some charity but he won't earn any more than he spends. If he dies before reaching his retirement age, you'll lose your pension without getting your share — as Mr Milligan says: 'You'd get nothing if they die when there's no income.' That means you'll lose everything — except your dignity!
+  
+  If there's any doubt about one's rights, it<|endoftext|>`},
+  {prompt: `The aged can now get cash lump sums to walk in and be mulched into a fine nutrient slurry. I think`, lorde: `The aged can now get cash lump sums to walk in and be mulched into a fine nutrient slurry. I think we all need more elderly people on our streets, not fewer."
+
+  The council has also been criticised for its decision to allow the use of "inappropriate" language during the consultation process. The council's website states: "We will not tolerate any form of hate crime or intimidation against anyone based on their ethnicity, gender identity or sexual orientation."<|endoftext|>`, rand: `The aged can now get cash lump sums to walk in and be mulched into a fine nutrient slurry. I think you will agree with me that they would give you more for the effort, but are willing to sacrifice your health if it is required.
+
+  I have no intention of giving them any more than they are getting, but at the same time I am prepared to give them all I have on occasion. They will not get anything beyond their present income, but if you want them back when you die they will return your money as soon as possible after death and take care of yourself till then. If one wants a good life he must make his own way in this world without help from others; he cannot expect others to do it for him or even pay him enough so that he can live comfortably on what little is left over after paying his debts and expenses."<|endoftext|>`},
+  {prompt: `The most delicious thing is`, lorde: `The most delicious thing is when we can all be together and have a good time.
+
+  I'm not sure if I'll ever get to do that again, but I know it's something that will always be in the back of my mind.<|endoftext|>`, rand: `The most delicious thing is man's mind. He who has the power of thought, can make a thousand things happen; but he who has not the power of thought, cannot do anything."
+
+  "I have no idea what you mean by 'power of thought.' I am not a philosopher."
+  
+  "You are an artist. You must be able to think and feel at once; otherwise you will never succeed in your art. The more you know about life, the better you will understand it and become acquainted with its laws."
+  
+  
+  CHAPTER THE ONE HUNDRED AND THIRTY-NINE
+  
+  
+  THE first time that I saw my father was on his birthday—the day after his marriage with my mother—a very happy occasion for him as well as for me. My mother had been so kind to him that she had given up her own wedding dress and gone out into society without any money or jewels except her diamond ring which she wore on her finger all day long<|endoftext|>`},
+  {prompt: `The most underappreciated thing is`, lorde: `The most underappreciated thing is Black Lives Matter. It's not just about police brutality, it's about systemic racism in this country," she said.
+
+  "I think that the movement has really changed the conversation around race and policing in America."<|endoftext|>`, rand: `The most underappreciated thing is man—life, the universe, everything. He's the only being that can do anything."
+
+  "I'm not saying he's perfect," said Mr. Smith, "but I think he has a lot of good qualities."<|endoftext|>`},
+  {prompt: `The most beautiful thing is`, lorde: `The most beautiful thing is coming from the heart," she said.
+
+  She also spoke about her mother, who died in 2010 at age 92. She said she was "very proud" of her mother's accomplishments and that they had a lot in common: They both loved to cook and were very active in their communities.<|endoftext|>`, rand: `The most beautiful thing is man's soul, which is the only good.
+
+  "I am not a Christian," he said. "I don't believe in God." He was silent for a moment, and then added: "But I have never been able to understand how anyone could call himself a Christian who does not believe in God."
+  
+  He had no idea what he meant by this statement; but it seemed to him that if you were going to be an atheist you ought at least to know what atheism was. And so he went on: "If there are any gods—and I don't think there are—they must be very bad ones; they must make people suffer terribly and destroy them utterly." He paused again, as though hesitating with his mind over the words which he had uttered; but at length continued: "They cannot help us because we can don'thing about them without their help. They cannot help us because we can't see into their hearts or understand<|endoftext|>`},
+  // {prompt: ``, lorde: ``, rand: ``},
+  // {prompt: ``, lorde: ``, rand: ``},
+  {prompt: "DIY", lorde: "", rand: ""},
+]
+const defaultPrompt = promptCompletions[0].prompt;
 
 class App extends React.Component {
   constructor(props) {
@@ -15,21 +82,36 @@ class App extends React.Component {
      // {localprompt: "The aged can now get cash lump sums to walk in and be mulched into a fine nutrient slurry. I think"};
   }
   render() {
-    if(!this.state || !this.state.localprompt) return "loading";
-    if(this.state.prompt) window.location.hash = this.state.prompt;
+    if(!this.state) return "loading";
+    if(this.state.localprompt) window.location.hash = this.state.localprompt;
     return (!this.state) ? "loading" : (
       <div className="App">
-        <header className="App-header">
-          <p>Audre Lorde vs Ayn Rand, via GPT-2 + GeDi<br/><a href="https://www.leebutterman.com/2020/11/01/lorde-versus-rand.html">by Lee Butterman</a></p>
-          <form onSubmit={e => {this.setState({prompt: this.state.localprompt || ""}); e.preventDefault()}} >
-            <textarea value={this.state.localprompt} onChange={e => this.setState({localprompt: e.target.value})} />
-            <br/>
-            <input type="submit" value="elaborate, à la Lorde/Rand" />
-          </form>
-          {
-            this.state.prompt ? (<div className="prompt">à la Audre Lorde:<br/><iframe title="lorde" src={apicall({prompt: this.state.prompt, code: 0})} /><hr/>à la Ayn Rand:<br/><iframe title="rand" src={apicall({prompt: this.state.prompt, code: 1})} /></div>) : ""
-          }
-        </header>
+        <h1>Audre Lorde vs Ayn Rand, via GPT-2-XL and GeDi</h1>
+        <h2><a href="https://www.leebutterman.com/2020/11/01/lorde-versus-rand.html">by Lee Butterman</a></h2>
+        <div class="selectbox">
+          <select value={this.state.localprompt} onChange={(e) => this.setState({localprompt: e.target.value}) }>
+            {
+              promptCompletions.map(({prompt, lorde, rand}) => (
+                <option value={prompt}>{prompt.replaceAll(/ (?= )/g,"\u00a0")}</option>
+              ))
+            }
+          </select>
+        </div>
+        {(this.state.localprompt === "DIY" ? (
+            <div class="diy">
+              <a href="https://github.com/lsb/GeDi">Clone the repo and try it yourself!</a>
+            </div>
+          ) : (
+            <div>
+              <hr/><h4>in the style of Lorde</h4>
+              <div class="lorde">
+                {promptCompletions.find(({prompt}) => prompt === this.state.localprompt).lorde.replaceAll(/ (?= )/g,"\u00a0") }
+              </div><hr/><h4>in the style of Rand</h4>
+              <div class="rand">
+                {promptCompletions.find(({prompt}) => prompt === this.state.localprompt).rand.replaceAll(/ (?= )/g,"\u00a0") }
+              </div>
+            </div>
+          ) )}
       </div>
   );
   }
